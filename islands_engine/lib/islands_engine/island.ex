c9632fa732e6a05505e529ca841bf2a74acf1b40
Vipul Sharma <vipulsharma018@gmail.com>
2018-03-18 02:25:24 -0400

    Island overlap?

--- islands_engine/lib/islands_engine/island.ex
+++ islands_engine/lib/islands_engine/island.ex
@@ -58,4 +58,8 @@ defmodule IslandsEngine.Island do
     end
   end
 
+  # function to check if the island's overlap each other (Additioanl backend check)
+  def overlaps?(existing_island, new_island), do:
+    not MapSet.disjoint?(existing_island.coordinates,new_island.coordinates)
+
 end
