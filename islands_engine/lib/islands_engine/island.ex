331c4f5f3cab21046a5ad65898c1ffcd8f8aed32
Vipul Sharma <vipulsharma018@gmail.com>
2018-03-17 22:20:10 -0400

    Island creation master file

---
+++ islands_engine/lib/islands_engine/island.ex
@@ -0,0 +1,61 @@
+defmodule IslandsEngine.Island do
+  alias IslandsEngine.{Coordinate, Island}
+
+  @enforce_keys [:coordinates, :hit_coordinates]
+  defstruct [:coordinates, :hit_coordinates]
+
+  # using MapSet as at the time of checking the order of insertion won't matter
+  # Example:
+  # iex> MapSet.equal?(MapSet.new([1,2]), MapSet.new([2,1]))
+  # true
+  # def new(), do:
+  #   %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
+
+  def new(type, %Coordinate{} = upper_left) do
+    with [_|_] = offsets <- offsets(type),
+         %MapSet{} = coordinates <- add_coordinates(offsets, upper_left)
+    do
+      {:ok, %Island{coordinates: coordinates, hit_coordinates: MapSet.new()}}
+    else
+      error -> error
+    end
+  end
+
+  # square shaped islands
+  defp offsets(:square), do: [{0,0},{0,1},{1,0},{1,1}]
+
+  # atoll shaped islands
+  defp offsets(:atoll), do: [{0,0},{0,1},{1,1},{2,0},{2,1}]
+
+  # simple dot shaped islands
+  defp offsets(:dot), do: [{0,0}]
+
+  # l-shaped islands
+  defp offsets(:l_shape), do: [{0,0},{1,0},{2,0},{2,1}]
+
+  # s-shaped islands
+  defp offsets(:s_shape), do: [{0,1},{0,2},{1,0},{1,1}]
+
+  # all other shaped islands
+  defp offsets(_), do: {:error, :invalid_island_type}
+
+  # returns either the complete set of coordinates or an error tuple,
+  # if we get an invalid island coordinate
+  defp add_coordinates(offsets, upper_left) do
+    # Enum.reduce will not work as it cannot handle invalid coordinate inputs
+    Enum.reduce_while(offsets, MapSet.new(), fn offset, acc ->
+      add_coordinate(acc, upper_left, offset)
+    end)
+  end
+
+  # check to see if a coordinate is valid
+  defp add_coordinate(coordinates, %Coordinate{row: row, col: col}, {row_offset, col_offset}) do
+    case Coordinate.new(row + row_offset, col + col_offset) do
+      {:ok, coordinate} ->
+        {:cont, MapSet.put(coordinates,coordinate)}
+      {:error, :invalid_coordinate} ->
+        {:halt,{:error, :invalid_coordinate}}
+    end
+  end
+
+end
