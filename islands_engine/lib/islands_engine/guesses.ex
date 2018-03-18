a9ca9ecfe7c498d594088b96914b30f7e583f596
Vipul Sharma <vipulsharma018@gmail.com>
2018-03-18 02:16:48 -0400

    Hit or Miss

--- islands_engine/lib/islands_engine/guesses.ex
+++ islands_engine/lib/islands_engine/guesses.ex
@@ -1,5 +1,6 @@
 defmodule IslandsEngine.Guesses do
   alias __MODULE__
+  alias IslandsEngine.{Coordinate, Guesses}
 
   @enforce_keys [:hits, :misses]
   defstruct [:hits, :misses]
@@ -7,5 +8,13 @@ defmodule IslandsEngine.Guesses do
   # Using MapSet to enforce automatic uniqueness of data
   def new(), do:
     %Guesses{hits: MapSet.new(), misses: MapSet.new()}
+
+  # When the guess hits a target ship/island
+  def add(%Guesses{} = guesses, :hit, %Coordinate{} = coordinate), do:
+    update_in(guesses.hits, &MapSet.put(&1, coordinate))
+
+  # When the guess misses a target ship/island
+  def add(%Guesses{} = guesses, :miss, %Coordinate{} = coordinate), do:
+  update_in(guesses.misses, &MapSet.put(&1, coordinate))
 
 end
